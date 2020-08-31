6b8f70f5a6175cb463e499972fbfd26078504fa5
MattRowling <51064823+MattRowling@users.noreply.github.com>
2020-08-31 19:18:16 +1200

    Update Room.java

--- Room.java
+++ Room.java
@@ -3,11 +3,21 @@
 public class Room extends Card {
     private String name;
 
+    /*
+    * Constructor for the room card
+    * 
+    * @param roomName
+    */
     public Room(String roomName) {
         super(roomName);
         name = roomName;
     }
 
+    /*
+    * Gets the name of the room
+    * 
+    * @return String
+    */
     public String getName() {
         return name;
     }
